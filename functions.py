6b5a6ee2fcde228176a83094e4bf7f8cc6f1c72f
FernandoVelarde <fvelarde@csumb.edu>
2024-12-10 16:25:56 -0800

    Commented Ingredient Search
    I put a few comments to help better understand what is happening.

--- functions.py
+++ functions.py
@@ -1,6 +1,7 @@
 import requests, json
 from pprint import pprint
 
+# This is what we did in class, but it's a function for any other future api we will need.
 def ingredient_api(endpoint, params=None):
     try:
         r = requests.get(endpoint, params)
@@ -12,15 +13,22 @@ def ingredient_api(endpoint, params=None):
     except:
         pprint('please try again')
 
+# This is the functions that gets the needed information from the api.
+# This will then get passed onto the website getting shown by flask.
 def search_ingredient(ingredients):
     recipes = []
-    
+        
     for recipe in ingredients:
+# The next three lines gets from the dictionary, the meal name, image, and recipe id.
         meal_name = recipe.get('title', 'Unknown Meal')
         image = recipe.get('image', 'No Image Available')
-        
+        recipe_id = recipe.get('id', 'Unknown ID')
+
+# This gets all the ingredients that were typed in and ingredients that were not typed in.
         all_ingredients = recipe.get('missedIngredients', []) + recipe.get('usedIngredients', [])
         
+# This list loops through the ingredients and gets measurements and organizes it to its corresponding ingredient.
+# The if statement ensures it is getting from a valid key.
         ingredients = [
             f"{item['amount']} {item['unit']} {item['name']}".strip() 
             for item in all_ingredients
@@ -30,7 +38,8 @@ def search_ingredient(ingredients):
         recipes.append({
             'meal_name': meal_name,
             'ingredients': ingredients,
-            'image': image
+            'image': image,
+            'recipe_id': recipe_id
         })
     
     return recipes
